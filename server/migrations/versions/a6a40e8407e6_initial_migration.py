7c24602ca0f026b5aff25b6bc41a8256c8563b95
Mustafa Mehmedoglu <mikbalm@gmail.com>
2025-05-06 22:46:12 -0400

    code-along!

--- server/migrations/versions/a6a40e8407e6_initial_migration.py
+++ server/migrations/versions/a6a40e8407e6_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration.
 
-Revision ID: c6714e810146
+Revision ID: a6a40e8407e6
 Revises: 
-Create Date: 2023-08-23 11:16:32.627596
+Create Date: 2025-05-06 22:37:37.938088
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c6714e810146'
+revision = 'a6a40e8407e6'
 down_revision = None
 branch_labels = None
 depends_on = None
